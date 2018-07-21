61d036f892e30311a75604b4a3f6520bffa281f9
stevetaggart <steve.taggart@gmail.com>
2018-07-20 21:25:14 -0600

    Formatting.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,15 +5,15 @@ class QuizQuestion extends Component {
     render() {
         return (
             <main>
-            <section>
-              <p>{this.props.quiz_question.instruction_text}</p>
-            </section>
-            <section className="buttons">
-              <ul>
-                <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
-              </ul>
-            </section>
-          </main>
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+                    </ul>
+                </section>
+            </main>
         )
     }
 }
