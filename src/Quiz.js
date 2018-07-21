1c930a1742955686812d6a4b1e8225bb58472b33
stevetaggart <steve.taggart@gmail.com>
2018-07-20 22:56:27 -0600

    Display quiz questions and handle events

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,12 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 }
     }
 
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1 }
+        })
+    }
+
     render() {
         const isQuizEnd = this.state.quiz_position - 1 == quizData.quiz_questions.length;
         if (isQuizEnd) {
@@ -21,7 +27,8 @@ class Quiz extends Component {
         }
         return (
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+                    showNextQuestionHandler={this.showNextQuestion.bind(this)} />
             </div>
         )
     }
